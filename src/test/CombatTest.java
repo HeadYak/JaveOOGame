e0c62f03002248681c9b91d19f7ba3308c52a407
Elijah Lee <elijah.lee@student.unsw.edu.au>
2021-07-17 17:29:27 +1000

    changed world path and dimensions

--- src/test/CombatTest.java
+++ src/test/CombatTest.java
@@ -16,11 +16,18 @@ import unsw.loopmania.Character;
 public class CombatTest {
     List<Pair<Integer, Integer>> path = Arrays.asList(
             new Pair<>(0, 1), new Pair<>(0, 2), new Pair<>(0, 3),
-            new Pair<>(1, 3), new Pair<>(2, 3), new Pair<>(3, 3),
-            new Pair<>(3, 2), new Pair<>(3, 1), new Pair<>(3, 0),
-            new Pair<>(2, 0), new Pair<>(1, 0), new Pair<>(0, 0));
+            new Pair<>(0, 4), new Pair<>(0, 5), new Pair<>(0, 6),
+            new Pair<>(0, 7), new Pair<>(0, 8), new Pair<>(1, 8),
+            new Pair<>(2, 8), new Pair<>(3, 8), new Pair<>(4, 8),
+            new Pair<>(5, 8), new Pair<>(6, 8), new Pair<>(7, 8),
+            new Pair<>(8, 8), new Pair<>(8, 7), new Pair<>(8, 6),
+            new Pair<>(8, 5), new Pair<>(8, 4), new Pair<>(8, 3),
+            new Pair<>(8, 2), new Pair<>(8, 1), new Pair<>(8, 0),
+            new Pair<>(7, 0), new Pair<>(6, 0), new Pair<>(5, 0),
+            new Pair<>(4, 0), new Pair<>(3, 0), new Pair<>(2, 0),
+            new Pair<>(1, 0), new Pair<>(0, 0));
 
-    LoopManiaWorld world = new LoopManiaWorld(4, 4, path);
+    LoopManiaWorld world = new LoopManiaWorld(9, 9, path);
 
     @Test
     public void testBattleWithSlug() {
