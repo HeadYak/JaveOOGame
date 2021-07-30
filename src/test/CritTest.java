6f3eb89b9b82708cb0ffd9066081bfa95d6c75d2
Elijah Lee <elijah.lee@student.unsw.edu.au>
2021-07-31 01:35:42 +1000

    finished all tests but it3

--- src/test/CritTest.java
+++ src/test/CritTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +17,8 @@ import unsw.loopmania.Items.Weapons.Stake;
 import unsw.loopmania.Items.Weapons.Sword;
 import unsw.loopmania.enemies.Slug;
 import unsw.loopmania.enemies.Vampire;
+import unsw.loopmania.enemies.Zombie;
+import unsw.loopmania.Ally;
 import unsw.loopmania.BattleManager;
 import unsw.loopmania.Character;
 
@@ -119,7 +122,7 @@ public class CritTest {
         Character playerChar = new Character(charP);
         world.setCharacter(playerChar);
 
-        // Create new slug on same tile as player
+        // Create new vampire on same tile as player
         Vampire vampire = new Vampire(charP, world);
         vampire.setDmg(0);
         world.addEnemy(vampire);
@@ -165,11 +168,124 @@ public class CritTest {
 
     @Test
     public void testZombieCrits() {
+        PathPosition charP = new PathPosition(0, path);
+        Character playerChar = new Character(charP);
+        world.setCharacter(playerChar);
 
+        // Give character two allies
+        Ally ally1 = new Ally(charP);
+        Ally ally2 = new Ally(charP);
+        playerChar.recruitAlly(ally1);
+        playerChar.recruitAlly(ally2);
+        assertEquals(playerChar.getAllyList().size(), 2);
+
+        // Create new zombie on same tile as player
+        Zombie zombie = new Zombie(charP, playerChar);
+        world.addEnemy(zombie);
+
+        // Simulate battle manually to test each component of battle is working
+        // correctly and set crit chance to 100%
+        BattleManager bm = world.getBattleManager();
+        bm.setCritMode(2);
+
+        bm.update(world);
+
+        // Check that there is 1 zombie
+        assertEquals(bm.getBattleEnemies().size(), 1);
+
+        // Do a single tick of battle
+        bm.runTickBattle();
+
+        // Check that the character received expected damage (extra 2x dmg)
+        int newCharHp = 300 - (zombie.getDmg() * 8);
+        assertEquals(playerChar.getHp(), newCharHp);
+
+        // Check that the allies has reduced to 1
+        assertEquals(playerChar.getAllyList().size(), 1);
+
+        // Check that there are two zombies
+        assertEquals(bm.getBattleEnemies().size(), 2);
     }
 
     @Test
     public void testVampireCrits() {
+        PathPosition charP = new PathPosition(0, path);
+        Character playerChar = new Character(charP);
+        world.setCharacter(playerChar);
+
+        // Create new vampire on same tile as player
+        Vampire vampire = new Vampire(charP, world);
+        world.addEnemy(vampire);
+
+        // Simulate battle manually to test each component of battle is working
+        // correctly and set crit chance to 100%
+        BattleManager bm = world.getBattleManager();
+        bm.setCritMode(2);
+
+        bm.update(world);
+
+        // Do a single tick of battle
+        bm.runTickBattle();
+
+        // Check that the vampire's buff are in the correct range
+        assertTrue(vampire.getBuffDuration() >= 1 &&
+                vampire.getBuffDuration() <= 5);
+        assertTrue(vampire.getBuffDmg() >= 5 &&
+                vampire.getBuffDuration() <= 15);
+
+        // Check that the character received expected damage range (extra 2x dmg)
+        int newCharHp = 300 - (vampire.getDmg() * 8) - (vampire.getBuffDmg());
+        assertEquals(playerChar.getHp(), newCharHp);
+    }
+
+    @Test
+    public void testRandomVampireConsistency() {
+        PathPosition charP = new PathPosition(0, path);
+        Character playerChar = new Character(charP);
+        world.setCharacter(playerChar);
+
+        // Create new vampire on same tile as player
+        Vampire vampire = new Vampire(charP, world);
+        world.addEnemy(vampire);
+
+        // Simulate battle manually to test each component of battle is working
+        // correctly and set crit chance to 100%
+        BattleManager bm = world.getBattleManager();
+        bm.setCritMode(2);
+
+        bm.update(world);
+
+        // Loop 100 times and check that vampire buff stats satisfy range all
+        // 100 times
+        for (int i = 0; i < 100; i++) {
+            playerChar.regen(playerChar.getMaxHp());
+            bm.runTickBattle();
+
+            // Check that the vampire's buff are in the correct range
+            assertTrue(vampire.getBuffDuration() >= 1 &&
+                    vampire.getBuffDuration() <= 5);
+            assertTrue(vampire.getBuffDmg() >= 5 &&
+                    vampire.getBuffDuration() <= 15);
+
+            vampire.setBuffDuration(0);
+            vampire.setBuffDmg(0);
+            vampire.setHp(vampire.getMaxHp());
+        }
+
+        // Test that buff stacks
+        bm.runTickBattle();
+
+        assertTrue(vampire.getBuffDuration() >= 1 &&
+                vampire.getBuffDuration() <= 5);
+        assertTrue(vampire.getBuffDmg() >= 5 &&
+                vampire.getBuffDuration() <= 15);
+
+        bm.runTickBattle();
+        
+        assertTrue(vampire.getBuffDuration() >= 1 &&
+                vampire.getBuffDuration() <= 9);
+        assertTrue(vampire.getBuffDmg() >= 10 &&
+                vampire.getBuffDuration() <= 30);
 
     }
 
@@ -180,17 +296,6 @@ public class CritTest {
 
     @Test
     public void testElanMuskeCrits() {
-        
-    }
-
-    @Test
-    public void testClassSpecificDmgModifier() {
 
     }
-
-    @Test
-    public void testSpecialEffects() {
-
-    }
-
 }
